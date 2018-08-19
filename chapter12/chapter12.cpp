3ee307ffe3c13df740beefb6a309caa7e822989b
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-08-19 12:49:56 -0700

    implement a function which reverses a null-terminated string in C++

--- chapter12/chapter12.cpp
+++ chapter12/chapter12.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-#include <string>
-#include <fstream>
 using namespace std;
 
+/*
+Reverse String: Implement a function void reverse(char* str)
+in C or C++ which reverses a null-terminated string.
+*/
+void reverseString(char * str);
+
 int main(){
-    ;
+    string s = "Hello World.";
+    char * str = new char[s.size()];
+    s.copy(str, s.size());
+    
+    reverseString(str);
+
+    cout<<str<<endl;
+}
+
+void reverseString(char * str){
+    int size = strlen(str);
+    for( int i = 0; i < size/2; i++ ){
+        char temp = str[i];
+        str[i] = str[size-i-1];
+        str[size-i-1] = temp;
+    }
 }
