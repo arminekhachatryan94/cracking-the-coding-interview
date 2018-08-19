8fe8ed40253b166a6edff57f6f430ff1eeba0390
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-08-18 21:36:23 -0700

    given a binary tree, create a linked list of all the nodes at each depth

--- chapter4/chapter4.cpp
+++ chapter4/chapter4.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <list>
 using namespace std;
+
+/*
+List of Depths: Given a binary tree, design an algorithm which creates
+a linked list of all the nodes at each depth (e.g., if you have a tree
+with depth D, you'll have Dlinked lists).
+*/
 
 struct Node{
     int label;
@@ -9,15 +16,23 @@ struct Node{
     Node(): left(NULL), right(NULL){};
 };
 
-Node * MinimalTree( vector<int> subArr);
+Node * MinimalTree(vector<int> subArr);
+list<Node> * ListOfDepths(Node * tree, int & length);
+void Traverse(Node * tree, list<Node> * & depths, int depth, int & length);
+void printList(list<Node> * depths, int length);
 
 int main(){
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
     vector<int> a(arr, arr+sizeof(arr)/sizeof(int));
     Node * tree = MinimalTree(a);
+
+    int length = 0;
+    list<Node> * d = ListOfDepths(tree, length);
+
+    printList(d, length);
 }
 
-Node * MinimalTree( vector<int> subArr){
+Node * MinimalTree(vector<int> subArr){
     int size = subArr.size();
     Node * tree;
     tree = new Node;
@@ -39,4 +54,39 @@ Node * MinimalTree( vector<int> subArr){
     }
 
     return tree;
+}
+
+list<Node> * ListOfDepths(Node * tree, int & length){
+    int depth = 0;
+    list<Node> * DList;
+    Traverse(tree, DList, depth, length);
+    return DList;
+}
+
+void Traverse(Node * tree, list<Node> * & depths, int depth, int & length){
+    if( tree != NULL ){
+        if( length <= depth ){
+            list<Node> * temp = new list<Node>[depth+1];
+            for( int i = 0; i < length; i++ ){
+                temp[i] = depths[i];
+            }
+            depths = temp;
+            length = depth+1;
+        }
+        (*(depths+depth)).push_back(*tree);
+        Traverse(tree->left, depths, depth+1, length);
+        Traverse(tree->right, depths, depth+1, length);
+    }
+}
+
+void printList(list<Node> * depths, int length){
+    for( int i = 0; i < length; i++ ){
+        cout<<"depth "<<i<<": ";
+        for( int j = 0; j < (*(depths+i)).size(); j++ ){
+            cout<<(*(depths+i)).front().label<<" ";
+            (*(depths+i)).push_back((*(depths+i)).front());
+            (*(depths+i)).pop_front();
+        }
+        cout<<endl;
+    }
 }
