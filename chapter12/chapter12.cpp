291047e33742fadbf0a0a3fad6b6e62a86b63f1b
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-08-19 11:19:47 -0700

    print last K lines of input file using C++

--- chapter12/chapter12.cpp
+++ chapter12/chapter12.cpp
@@ -3,6 +3,42 @@
 #include <fstream>
 using namespace std;
 
+/*
+Last K Lines: Write a method to print the last K lines of an input file using C++.
+*/
+void LastKLines(string path, int K);
+
 int main(){
-    ;
+    string path = "12.1_input.txt";
+    LastKLines(path, 10);
+}
+
+void LastKLines(string path, int K){
+    ifstream input(path);
+    if( !input ){
+        cout<<"Error opening input file"<<endl;
+        exit(1);
+    } else {
+        // count number of lines in input file
+        int num_lines = 0;
+        string line;
+        while (getline(input, line)) {
+            num_lines++;
+        }
+
+        // go back to beginning of input file
+        input.clear();
+        input.seekg(0, ios::beg);
+
+        // print last K lines
+        int index = 0;
+        while (getline(input, line)) {
+            if( num_lines <= K || index >= num_lines-K){
+                cout<<line<<endl;
+            }
+            index++;
+        }
+
+        input.close();
+    }
 }
