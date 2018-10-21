e39c4e5fce86729a4c20cc9d1af1921043343e93
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-10-21 16:32:39 -0700

    given J (string of jewels) and S (stones you own), find the number of stones you have that are jewels

--- more_problems/Practice.java
+++ more_problems/Practice.java
@@ -7,18 +7,23 @@ class Practice {
         int[] A1 = { -1, 0, 1, 2, 3 };
         int[] A2 = {-1, 2, 4, 1, 0 };
 
-        int[] S1 = solution(2, A1);
-        int[] S2 = solution(3, A2);
+        int[] S1 = parentD(2, A1);
+        int[] S2 = parentD(3, A2);
 
         System.out.println(Arrays.toString(S1));
         System.out.println(Arrays.toString(S2));
+
+
+        String J = "aA", S = "aAAbbbb";
+        int count = numJewelsInStones(J, S);
+        System.out.println("Jewels: " + J + ", Stones: " + S + ", count: " + count);
     }
 
     /*
     Given an integer D and an array of integers A, find the
     Dth parent of every element in A
     */
-    public static int[] solution(int D, int[] A){
+    public static int[] parentD(int D, int[] A){
         int[] ret = new int[A.length];
         HashMap<Integer, Integer> hash = new HashMap<>();
 
@@ -50,5 +55,27 @@ class Practice {
         }
 
         return ret;
+    }
+
+    /*
+    Given two strings, J and S:
+    J represents the type of stones that are jewels,
+    and S represents the stones you have
+    Find the number of stones you have that are jewels.
+    */
+    public static int numJewelsInStones(String J, String S) {
+        HashMap<Character, Integer> jewels = new HashMap<>();
+        for( int jewel = 0; jewel < J.length(); jewel++ ){
+            jewels.put(J.charAt(jewel), jewel);
+        }
+
+        int count = 0;
+        for( int stone = 0; stone < S.length(); stone++ ){
+            if( jewels.get(S.charAt(stone)) != null ){
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
